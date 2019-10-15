1b94b575b0ddc2622ce19864ed3387a1bfb8f022
Sergey Lukin <sergey@hiredscore.com>
2019-10-15 17:51:57 +0300

    Front: add page transition progress bar animation

--- front/gatsby-config.js
+++ front/gatsby-config.js
@@ -14,6 +14,15 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: true,
+      },
+    },
+    {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: process.env.DEPLOY_URL
