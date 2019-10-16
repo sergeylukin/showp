dae4b8201bd220dd7aed32a286cf1756598b1f5b
Sergey Lukin <sergey@hiredscore.com>
2019-10-16 04:59:55 +0300

    Front: styling with Grommet/styled-components

--- front/src/components/header.js
+++ front/src/components/header.js
@@ -1,54 +1,59 @@
 import PropTypes from "prop-types"
 import React from "react"
+import { Box } from 'grommet';
 
 import locales from '../constants/locales'
 import Link from "../components/localizedLink"
 import usePath from "../hooks/usePath"
 import useLocale from "../hooks/useLocale"
 
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='light-1'
+    pad={{ vertical: 'small', horizontal: 'medium' }}
+    elevation='medium'
+    {...props}
+  />
+)
+
 const Header = ({ siteTitle }) => {
   const { localessPath } = usePath()
   const { currentLocale } = useLocale()
 
   return (
-    <header
-      style={{
-        background: `white`,
-        marginBottom: `1.45rem`,
-      }}
-    >
-      <div
-        style={{
-          margin: `0 auto`,
-          padding: `.8rem 1.0875rem`,
-          boxShadow: '0 1px 2px 0 rgba(31,45,61,.15)',
-        }}
-      >
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to="/"
-            style={{
-              textDecoration: `none`,
-            }}
-          >
-            {siteTitle}
-          </Link>
-          
-          {Object.keys(locales).map(locale => {
-            const item = locales[locale]
-            const isCurrentLocale = locale === currentLocale
-            return (
-              <Link to={localessPath} locale={locale}>
-                {isCurrentLocale ? (
-                <big>{item.title}</big>
-                ) : (
-                  <small>{item.title}</small>
-                )}
-              </Link>
-              )
-          })}
-        </h1>
-      </div>
+    <header>
+      <AppBar>
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                textDecoration: `none`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+            
+            <Box direction='row'>
+              {Object.keys(locales).map(locale => {
+                const item = locales[locale]
+                const isCurrentLocale = locale === currentLocale
+                return (
+                  <Link to={localessPath} locale={locale}>
+                    {isCurrentLocale ? (
+                    <big>{item.title}</big>
+                    ) : (
+                      <small>{item.title}</small>
+                    )}
+                  </Link>
+                  )
+              })}
+            </Box>
+      </AppBar>
     </header>
   )
 }
--- front/src/components/layout.js
+++ front/src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import { IntlProvider } from 'react-intl'
 import Headroom from 'react-headroom'
+import { Grommet } from 'grommet';
 
 import Header from "./header"
 import "./layout.css"
@@ -30,6 +31,28 @@ const {
   Provider: PathProvider
 } = PathContext
 
+
+const theme = {
+  global: {
+    colors: {
+      'light-1': 'white',
+      'light-2': '#f5f5f5',
+      'text': {
+        light: 'rgba(0, 0, 0, 0.87)',
+      },
+    },
+    edgeSize: {
+      small: '14px',
+    },
+    elevation: {
+      light: {
+        // medium: '0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)',
+        medium: '0 1px 2px 0 rgba(31,45,61,.15)',
+      },
+    },
+  },
+};
+
 const Layout = ({ pageContext: {locale, localessPath},  children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -45,26 +68,28 @@ const Layout = ({ pageContext: {locale, localessPath},  children }) => {
     <IntlProvider locale={locale}  messages={messages[locale]}>
       <LocaleProvider value={{currentLocale: locale}}>
         <PathProvider value={{localessPath}}>
-          <Headroom
-            upTolerance={10}
-            downTolerance={10}
-            style={{zIndex: '20', height: '6.5em'}}
-          >
-            <Header siteTitle={data.site.siteMetadata.title} />
-          </Headroom>
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0,
-            }}
-          >
-            <main>{children}</main>
-            <footer>
-              © {new Date().getFullYear()}, Yay.tips
-            </footer>
-          </div>
+          <Grommet theme={theme}>
+            <Headroom
+              upTolerance={10}
+              downTolerance={10}
+              style={{zIndex: '20', height: '6.5em'}}
+            >
+              <Header siteTitle={data.site.siteMetadata.title} />
+            </Headroom>
+            <div
+              style={{
+                margin: `0 auto`,
+                maxWidth: 960,
+                padding: `0px 1.0875rem 1.45rem`,
+                paddingTop: 0,
+              }}
+            >
+              <main>{children}</main>
+              <footer>
+                © {new Date().getFullYear()}, Yay.tips
+              </footer>
+            </div>
+          </Grommet>
         </PathProvider>
       </LocaleProvider>
     </IntlProvider>
