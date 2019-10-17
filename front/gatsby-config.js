2086b15f76a2cbcf6ccfa99e195bde774f6121df
Sergey Lukin <sergey@hiredscore.com>
2019-10-17 12:29:31 +0300

    Front: add favicons

--- front/gatsby-config.js
+++ front/gatsby-config.js
@@ -48,13 +48,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Yay.tips`,
+        short_name: `Yay.tips`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#ffffff`,
+        theme_color: `#66d397`,
+        display: `standalone`,
+        icon: `src/images/yaytips-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
