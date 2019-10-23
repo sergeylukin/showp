1dbef1f5e37bff2d7295cdc87adcd9fe89f6744f
Sergey Lukin <sergey@hiredscore.com>
2019-10-23 11:22:25 +0300

    CMS: Post scheduled tips

--- cms/api/tip/controllers/Tip.js
+++ cms/api/tip/controllers/Tip.js
@@ -1,8 +1,12 @@
 'use strict';
+// import moment from 'moment';
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/guides/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  publish: async (ctx) => {
+  }
+};
--- cms/config/functions/cron.js
+++ cms/config/functions/cron.js
@@ -1,4 +1,6 @@
 'use strict';
+const axios = require('axios');
+const moment = require('moment');
 
 /**
  * Cron config that gives you an opportunity
@@ -16,4 +18,47 @@ module.exports = {
   // '0 1 * * 1': () => {
   //
   // }
+  '*/1 * * * *': async() => {
+    // console.log('I am running ' + new Date(), Object.keys(strapi.config));
+    // console.log(strapi.models.tip.query({
+    //   where: { isPublished: 'Yes' }
+    // })[0].title);
+    // await strapi.services.tip.publish();
+    return Tip.forge()
+      .where({
+        isPublished: 'No'
+      })
+      .fetchAll()
+      .then(function (tips) {
+
+        tips.forEach((tip) => {
+          const shouldBePublished = moment().diff(tip.get('publish_datetime'), 'seconds') > 0;
+          console.log(tip.get('title'));
+          if (shouldBePublished) {
+            console.log('Publishing ' + tip.get('id'));
+            tip
+              .set('isPublished', 'Yes')
+              .set('publish_datetime', moment().utc())
+              .save()
+              .then(() => {
+                const urls = strapi
+                  && strapi.config
+                  && strapi.config.staticWebsiteBuildURLs;
+                return axios.all(urls.map(l => axios.post(l, {})))
+                  .then(axios.spread(function (...res) {
+                    // all requests are now complete
+                    console.log('Sent triggers to Netlify');
+                  }))
+                  .catch(() => {
+                    // Ignore
+                  });
+              });
+          }
+        });
+      })
+      .catch(function (err) {
+      // res.status(500).json({error: true, data: {message: err.message}});
+      });
+
+  }
 };
