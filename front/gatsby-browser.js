f780b1d38359d9c06bd6a6877f9e007f910d12a1
Sergey Lukin <contact@sergeylukin.com>
2020-01-12 03:46:12 +0200

    Front: scroll back to saved position on route change

--- front/gatsby-browser.js
+++ front/gatsby-browser.js
@@ -6,3 +6,14 @@
 
 // You can delete this file if you're not using it
 require("typeface-source-sans-pro")
+
+exports.shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition
+}) => {
+  const currentPosition = getSavedScrollPosition(location)
+
+  window.scrollTo(...(currentPosition || [0, 0]))
+
+  return false
+}
